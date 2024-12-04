1f0b2df5afe7bed3907e603e8ef00478d07603e4
fahad-hasan-gsc <hasanfahad264@gmail.com>
2024-12-03 20:58:22 -0800

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MedicalGurdProject', 
+    version='0.0.0',
+    author='Md Fahad Hasan',
+    packages=find_packages(),  
+    install_requires=[],  
+)
